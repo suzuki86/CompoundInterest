42906422ebd533afd0432e9225a17afc6ef9fd76
suzuki86 <tsnr0001@gmail.com>
2016-03-20 20:05:02 +0900

    First commit

---
+++ CompoundInterest.php
@@ -0,0 +1,11 @@
+<?php
+
+class CompoundInterest {
+  public static function calculate($currentPrincipal, $years, $interestRate, $annualAddition) {
+    $result = $currentPrincipal;
+    for ($x = 1; $x <= $years; $x++) {
+      $result = $result * (1 + $interestRate) + $annualAddition;
+    }
+    return floor($result);
+  }
+}
